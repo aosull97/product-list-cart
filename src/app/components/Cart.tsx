1f3d8f655b2843fc0c5f219ef29fd3e7b065a7db
aosull97 <alicesarahosullivan@gmail.com>
2025-07-21 16:02:39 +0100

    Display item name, quantity and price in cart

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -1,9 +1,19 @@
-const Cart = ({ cartCount }: { cartCount: number }) => {
+interface CartItem {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+const Cart = ({ cartItems }: { cartItems: CartItem[] }) => {
+
+  const totalCartCount = cartItems.reduce((acc, curr) => acc + curr.quantity, 0)
+  const totalItemPrice = cartItems.reduce((acc, curr) => acc + curr.price * curr.quantity, 0)
+
   
   return (
     <div className="mx-6 mt-6 p-4 bg-white rounded-lg">
-      <h1 className=" font-bold text-xl text-red">Your Cart ({cartCount})</h1>
-      {cartCount === 0 ? 
+      <h1 className=" font-bold text-xl text-red">Your Cart ({totalCartCount})</h1>
+      {totalCartCount === 0 ? 
       
       <div className="flex flex-col gap-4 w-100% px-6 items-center">
       <img src="/images/illustration-empty-cart.svg" alt="empty cart" width={75} height={75} />
@@ -13,7 +23,26 @@ const Cart = ({ cartCount }: { cartCount: number }) => {
       
       : 
       
-      <p>Your cart is not empty</p>
+     <div>
+      {cartItems.map((item) => (
+        <div key={item.name}>
+          <div>
+            <div>
+              <p>{item.name}</p>
+            </div>
+            <div className="flex justify-between">
+              <p>{item.quantity}x</p>
+              <p>£{item.price.toFixed(2)}</p>
+              <p>£{totalItemPrice.toFixed(2)}</p>
+            </div>
+            
+            
+            
+          </div>
+        </div>
+      ))}
+     </div>
+      
       
       }
      
